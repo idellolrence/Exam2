b018eee2e0a9b0e612b8b1c0bd4141f9ced4dd30
Idel Lawrence Aranilla <106528291+idellolrence@users.noreply.github.com>
2022-10-25 13:53:28 +0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,14 @@
+public class Node {
+    
+    char Info;
+    
+    Node left;
+    Node right;
+    
+    public Node(char Info) {
+        this.Info = Info;
+        
+        left = null;
+        right = null;
+    }
+}
